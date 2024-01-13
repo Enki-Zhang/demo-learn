92ab661f3133340d3a3240037a13552a0e01a883
Enki-Zhang <17855378518@163.com>
2024-01-13 15:16:23 +0800

    springsecurity登陆

---
+++ src/main/java/com/enki/MainApplication.java
@@ -0,0 +1,16 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Enki
+ * @ClassName MainApplication
+ * @Description: TODO
+ * @Date 2024/1/11 17:00
+ * @Version 1.0
+ */
+@SpringBootApplication
+public class MainApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class, args);
+    }
+}
---
+++ src/main/java/com/enki/controller/AppController.java
@@ -0,0 +1,22 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @author Enki
+ * @ClassName AppController
+ * @Description: TODO
+ * @Date 2024/1/11 16:41
+ * @Version 1.0
+ */
+@Controller
+public class AppController {
+
+    @RequestMapping("/hello")
+    @ResponseBody
+    String home() {
+        return "Hello ,spring security!";
+    }
+}
